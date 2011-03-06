fc9ca6b202331615a4d373188f3d447c60e13c92
Sam Neubardt <samneubardt@gmail.com>
2011-03-06 17:20:01 -0500

    Interpolate between two colors as the minute passes.

--- clock.js
+++ clock.js
@@ -4,7 +4,13 @@ function clock(el, opts) {
     var options = {
         // the clock will be updated every tickInterval ms
         // lower numbers => smoother animation
-        tickInterval: 100
+        tickInterval: 100,
+        // [r,g,b]
+        // startColor - the color when the minute has just started
+        startColor: [0, 255, 0],
+        // endColor - the color when the minute has ended
+        //endColor: [245, 0, 61]
+        endColor: [255, 0, 0]
     }
 
     // override default options
@@ -17,13 +23,22 @@ function clock(el, opts) {
     var minutes = "<span name='clock.minutes'></span>";
     el.innerHTML = hours + " : " + minutes;
 
+    // this is where the magic happens
     function tick() {
+        function interpolateColor(start, end, pct) {
+            return  start + Math.floor((end - start) * pct);
+        }
         var d = new Date();
         var hours = el.firstChild;
         var minutes = el.lastChild;
 
         hours.innerHTML = d.getHours();
         minutes.innerHTML = d.getMinutes();
+        var pct = d.getSeconds() / 59;
+        var r = interpolateColor(options['startColor'][0], options['endColor'][0], pct);
+        var g = interpolateColor(options['startColor'][1], options['endColor'][1], pct);
+        var b = interpolateColor(options['startColor'][2], options['endColor'][2], pct);
+        minutes.style.color = "rgb("+r+","+g+","+b+")";
     }
 
     window.setInterval(tick, options['tickInterval']);
